dbf241828cd811f2bd0c95b036addb7c9158526a
vincentsig <47405610+vincentsig@users.noreply.github.com>
2020-09-08 19:20:26 -0400

    create phone and specification entities

---
+++ src/Entity/Phone.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PhoneRepository::class)
+ */
+class Phone
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stock;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Specification.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SpecificationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SpecificationRepository::class)
+ */
+class Specification
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $OS;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $storage;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sim;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $network;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $wifi;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $read_camera_resolution;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $front_camera_resolution;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $display_resolution;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $dimensions;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $weight;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOS(): ?string
+    {
+        return $this->OS;
+    }
+
+    public function setOS(string $OS): self
+    {
+        $this->OS = $OS;
+
+        return $this;
+    }
+
+    public function getStorage(): ?string
+    {
+        return $this->storage;
+    }
+
+    public function setStorage(string $storage): self
+    {
+        $this->storage = $storage;
+
+        return $this;
+    }
+
+    public function getSim(): ?string
+    {
+        return $this->sim;
+    }
+
+    public function setSim(string $sim): self
+    {
+        $this->sim = $sim;
+
+        return $this;
+    }
+
+    public function getNetwork(): ?string
+    {
+        return $this->network;
+    }
+
+    public function setNetwork(string $network): self
+    {
+        $this->network = $network;
+
+        return $this;
+    }
+
+    public function getWifi(): ?string
+    {
+        return $this->wifi;
+    }
+
+    public function setWifi(string $wifi): self
+    {
+        $this->wifi = $wifi;
+
+        return $this;
+    }
+
+    public function getReadCameraResolution(): ?string
+    {
+        return $this->read_camera_resolution;
+    }
+
+    public function setReadCameraResolution(string $read_camera_resolution): self
+    {
+        $this->read_camera_resolution = $read_camera_resolution;
+
+        return $this;
+    }
+
+    public function getFrontCameraResolution(): ?string
+    {
+        return $this->front_camera_resolution;
+    }
+
+    public function setFrontCameraResolution(string $front_camera_resolution): self
+    {
+        $this->front_camera_resolution = $front_camera_resolution;
+
+        return $this;
+    }
+
+    public function getDisplayResolution(): ?string
+    {
+        return $this->display_resolution;
+    }
+
+    public function setDisplayResolution(string $display_resolution): self
+    {
+        $this->display_resolution = $display_resolution;
+
+        return $this;
+    }
+
+    public function getDimensions(): ?string
+    {
+        return $this->dimensions;
+    }
+
+    public function setDimensions(string $dimensions): self
+    {
+        $this->dimensions = $dimensions;
+
+        return $this;
+    }
+
+    public function getWeight(): ?string
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(string $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+}

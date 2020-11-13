54e1e74b1e0da7b1f430583ac103ac8fd64ddc2f
vincentsig <47405610+vincentsig@users.noreply.github.com>
2020-11-12 19:31:34 -0500

    refactoring PhoneFixtures class

--- src/DataFixtures/PhoneFixtures.php
+++ src/DataFixtures/PhoneFixtures.php
@@ -11,7 +11,53 @@ class PhoneFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $listPhone = [
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        $phoneNames = call_user_func_array('array_merge', $this->getPhonelist());
+
+        for ($i = 0; $i < count($phoneNames); $i++) {
+            $phone = new Phone();
+
+            $phone->setName($phoneNames[$i]);
+            $phone->setBrand($this->getBrandName($phoneNames[$i], $this->getPhonelist()));
+            $phone->setPrice($faker->randomFloat(2, 390, 2300));
+            $phone->setStock($faker->randomFloat(0, 0, 3000));
+            $phone->setDescription($faker->text($maxNbChars = 200));
+            $phone->setOS(($this->getListOs()[mt_rand(0, 3)]));
+            $phone->setSim($this->getListSim()[mt_rand(0, 2)]);
+            $phone->setNetwork('LTE');
+            $phone->setWifi('Yes');
+            $phone->setRearCameraResolution($faker->randomElement([6, 8, 16, 38]) . ' MP');
+            $phone->setFrontCameraResolution($faker->randomElement([6, 8, 16, 38]) . ' MP');
+            $phone->setDimensions($faker->randomFloat(2, 5, 8)  . '(W) x' . $faker->randomFloat(2, 9, 12) .  '(H) x' . $faker->randomFloat(2, 0.6, 1.4) . '(D) cm');
+            $phone->setWeight($faker->numberBetween($min = 100, $max = 250) . ' gr');
+            $phone->setDisplayResolution('1080 x 2400');
+            $phone->setStorage($faker->randomElement(([32, 64, 128])));
+
+            $manager->persist($phone);
+            $manager->flush($phone);
+        }
+    }
+
+    /**
+     * Get the Brand of the phoneName
+     * @param string $phoneName
+     * @param array $listPhone
+     * @return string
+     */
+    private function getBrandName($phoneName, $listPhone): string
+    {
+        foreach ($listPhone as $key => $value) {
+            if (in_array($phoneName, $value)) {
+                return $key;
+            }
+        }
+    }
+
+    private function getPhonelist() : array
+    {
+        return [
             'Apple' => [
                 'iPhone 11 Pro', 'iPhone SE 2',
             ],
@@ -48,51 +94,15 @@ class PhoneFixtures extends Fixture
                 'Mi 9',
             ]
         ];
-
-        $listOs = ['Android', 'iOS', 'Tizen', 'Linux',];
-
-        $listSim = ['Nano SIM', 'Micro SIM', 'Standart SIM'];
-
-        $faker = Faker\Factory::create('fr_FR');
-
-        $phoneNames = call_user_func_array('array_merge', $listPhone);
-
-        for ($i = 0; $i < count($phoneNames); $i++) {
-            $phone = new Phone();
-
-            $phone->setName($phoneNames[$i]);
-            $phone->setBrand($this->getBrandName($phoneNames[$i], $listPhone));
-            $phone->setPrice($faker->randomFloat(2, 390, 2300));
-            $phone->setStock($faker->randomFloat(0, 0, 3000));
-            $phone->setDescription($faker->text($maxNbChars = 200));
-            $phone->setOS(($listOs[mt_rand(0, 3)]));
-            $phone->setSim($listSim[mt_rand(0, 2)]);
-            $phone->setNetwork('LTE');
-            $phone->setWifi('Yes');
-            $phone->setRearCameraResolution($faker->randomElement([6, 8, 16, 38]) . ' MP');
-            $phone->setFrontCameraResolution($faker->randomElement([6, 8, 16, 38]) . ' MP');
-            $phone->setDimensions($faker->randomFloat(2, 5, 8)  . '(W) x' . $faker->randomFloat(2, 9, 12) .  '(H) x' . $faker->randomFloat(2, 0.6, 1.4) . '(D) cm');
-            $phone->setWeight($faker->numberBetween($min = 100, $max = 250) . ' gr');
-            $phone->setDisplayResolution('1080 x 2400');
-            $phone->setStorage($faker->randomElement(([32, 64, 128])));
-
-            $manager->persist($phone);
-            $manager->flush($phone);
-        }
     }
 
-    /**
-     * Get the Brand of the phoneName
-     * @param string $phoneName
-     * @param array $listPhone
-     * @return string
-     */
-    private function getBrandName($phoneName, $listPhone): string
+    private function getListOs() : array
     {
-        foreach ($listPhone as $key => $value) {
-            if (in_array($phoneName, $value)) {
-                return $key;
-            }
-        }
+        return ['Android', 'iOS', 'Tizen', 'Linux',];
+    }
+
+    private function getListSim() : array
+    {
+        return ['Nano SIM', 'Micro SIM', 'Standart SIM'];
     }
 }
